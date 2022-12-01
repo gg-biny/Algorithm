e58009a6ca630f0c4996204078105b84db417f83
gg-biny <118068193+gg-biny@users.noreply.github.com>
2022-12-02 01:18:25 +0900

    [Bronze V] Title: A+B - 7, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,14 @@
+T = int(input())
+result = []
+for _ in range(T):
+    A,B = map(int,input().split())
+    result.append([A,B])
+
+i=1
+for A,B in result:
+    print("Case #",end='')
+    print(i,end='')
+    print(":",end=' ')
+    print(A+B)
+    i = i+1
+    
